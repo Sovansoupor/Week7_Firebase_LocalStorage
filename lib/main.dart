db19f51d562fc446ee7108dd974ce1567ed6416e
Sovansoupor <sovansouporeng@gmail.com>
2025-03-28 23:27:30 +0700

    BLA-204 – Implement the Local Storage repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:week_3_blabla_project/data/repository/local/local_ride_pref_repositor.dart';
 import 'ui/providers/ride_pref_provider.dart';
 import 'data/repository/mock/mock_locations_repository.dart';
 import 'data/repository/mock/mock_rides_repository.dart';
 import 'service/locations_service.dart';
 import 'service/rides_service.dart';
-import 'data/repository/mock/mock_ride_preferences_repository.dart';
+// import 'data/repository/mock/mock_ride_preferences_repository.dart';
 import 'ui/screens/ride_pref/ride_pref_screen.dart';
 import 'ui/theme/theme.dart';
 
@@ -19,7 +20,7 @@ void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
         create: (_) => RidesPreferencesProvider(
-            repository: MockRidePreferencesRepository())),
+            repository: LocalRidePreferencesRepository())),
   ], child: const MyApp()));
 }
 
